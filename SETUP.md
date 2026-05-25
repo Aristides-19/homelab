03d9f3e8504138709156d847b4ad8c1c3262c0bd
Arístides Pérez <aristp.2005@gmail.com>
2026-05-25 19:29:35 -0400

    fix: ensure ownership for /opt/homelab

--- SETUP.md
+++
@@ -1,3 +0,0 @@
-# Setup
-
-- Every mounted folder must be own by LXCs root user
--- proxmox/setup.sh
+++ proxmox/setup.sh
@@ -4,3 +4,5 @@ apt update && apt upgrade -y
 
 printf "net.ipv4.ip_forward = 1\nnet.ipv6.conf.all.forwarding = 1\n" | tee /etc/sysctl.d/99-ip-forwarding.conf
 sysctl -p /etc/sysctl.d/99-ip-forwarding.conf
+
+chown -R 100000:100000 /opt/homelab
