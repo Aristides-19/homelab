a0443a4c5801406a8d007d977d0ed3be83b85d0f
Arístides Pérez <aristp.2005@gmail.com>
2026-05-25 18:09:24 -0400

    fix: correct unbound setup script

--- gateway/unbound/set_root_servers.sh
+++ gateway/unbound/set_root_servers.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo mkdir -p /var/lib/unbound
-sudo curl -o /var/lib/unbound/root.key https://data.iana.org/root-anchors/root-anchors.xml
-sudo curl -o /etc/unbound/root.hints https://internic.net
-sudo chown -R unbound:unbound /var/lib/unbound/root.key /etc/unbound/root.hints
+mkdir -p /var/lib/unbound
+wget https://www.internic.net/domain/named.root -qO- | tee /var/lib/unbound/root.hints >/dev/null
+chown -R unbound:unbound /var/lib/unbound
 
+echo "0 0 1 */6 * root wget https://www.internic.net/domain/named.root -qO- | tee /var/lib/unbound/root.hints >/dev/null 2>&1" | tee /etc/cron.d/unbound-hints
+chmod 644 /etc/cron.d/unbound-hints
