f0a9532f12fd1f754055c3741c1dfc1cad467e85
Arístides Pérez <aristp.2005@gmail.com>
2026-05-25 20:50:17 -0400

    feat: add lxc_setup bash script

---
+++ proxmox/lxc_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt update && apt upgrade -y && apt install -y xclip micro && apt remove -y nano && apt autoremove -y
+
+mkdir -p ~/.config/micro
+cat <<EOF > ~/.config/micro/settings.json
+{
+    "clipboard": "terminal",
+    "tabsize": 2
+}
+EOF
+
+if ! grep -q "export EDITOR=micro" ~/.bashrc; then
+    echo "export EDITOR=micro" | tee -a ~/.bashrc
+    echo "Added micro as the default editor in ~/.bashrc"
+fi
+
+export EDITOR=micro
+source ~/.bashrc
+
+echo "LXC setup complete."
