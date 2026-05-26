bfc83fce5d2118703806649b0c7387b98d2745af
Arístides Pérez <aristp.2005@gmail.com>
2026-05-26 00:15:05 -0400

    feat: add pod configuration files for Vaultwarden setup

---
+++ 401-personal/vaultwarden/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /etc/containers/systemd/
+
+cp /opt/vaultwarden/pod.kube /etc/containers/systemd/vaultwarden.kube
+systemctl daemon-reload
+
+echo "Vaultwarden setup complete. Run `systemctl start vaultwarden` to start the pod."
