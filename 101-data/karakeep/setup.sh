bb38d65b4c8f8d77fa7da74595877f05b520ca99
Arístides Pérez <aristp.2005@gmail.com>
2026-05-25 21:39:41 -0400

    feat: add pod configuration files for Karakeep stack setup

---
+++ 101-data/karakeep/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /etc/containers/systemd/
+
+cp /opt/karakeep/pod.kube /etc/containers/systemd/karakeep.kube
+systemctl daemon-reload
+
+echo "Karakeep setup complete. Run `systemctl start karakeep` to start the pod."
