f912cb7e3ff80725d105a352e8fc73a98825a286
Arístides Pérez <aristp.2005@gmail.com>
2026-05-25 23:14:50 -0400

    feat: add pod configuration files for UptimeKuma setup

---
+++ 200-monitor/uptime-kuma/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /etc/containers/systemd/
+
+cp /opt/uptime-kuma/pod.kube /etc/containers/systemd/uptime-kuma.kube
+systemctl daemon-reload
+
+echo "Uptime-Kuma setup complete. Run `systemctl start uptime-kuma` to start the pod."
