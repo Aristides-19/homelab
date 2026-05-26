9bd0f795543295ad2cada72a907d07a041167724
Arístides Pérez <aristp.2005@gmail.com>
2026-05-26 11:53:35 -0400

    feat: add deployment configuration and setup script for Dawarich

---
+++ 401-personal/dawarich/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /etc/containers/systemd/
+
+cp /opt/dawarich/pod.kube /etc/containers/systemd/dawarich.kube
+systemctl daemon-reload
+
+echo "Dawarich setup complete. Run `systemctl start dawarich` to start the pod."
