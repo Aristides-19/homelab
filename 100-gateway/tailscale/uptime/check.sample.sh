785e3b198554db00035012b142ed6ac2ddb2010d
Arístides Pérez <aristp.2005@gmail.com>
2026-06-27 14:40:25 -0400

    fix(gateway): change icmp packets for tailscale uptime check to increase tunnel window negotiation time

--- 100-gateway/tailscale/uptime/check.sample.sh
+++ 100-gateway/tailscale/uptime/check.sample.sh
@@ -3,7 +3,7 @@
 TARGET_TAILNET_IP="0.0.0.0"
 KUMA_URL="https://uptime.change.me/api/push/UPTIME_TAILSCALE"
 
-PING_MS=$(ping -c 1 -W 2 "$TARGET_TAILNET_IP" | grep 'time=' | awk -F'time=' '{print $2}' | cut -d' ' -f1)
+PING_MS=$(ping -c 3 -W 2 "$TARGET_TAILNET_IP" | grep 'time=' | tail -n 1 | awk -F'time=' '{print $2}' | cut -d' ' -f1)
 
 if [ -n "$PING_MS" ]; then
     curl -s "${KUMA_URL}?status=up&msg=OK&ping=${PING_MS}" > /dev/null
