56c7cc430cd39ec503bd59c2d3969d3eaf9e293c
Arístides Pérez <aristp.2005@gmail.com>
2026-07-02 21:27:54 -0400

    feat remove dawarich service and associated configuration files

--- 401-personal/dawarich/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p /etc/containers/systemd/
-
-cp /opt/dawarich/pod.kube /etc/containers/systemd/dawarich.kube
-systemctl daemon-reload
-
-echo "Dawarich setup complete. Run \`systemctl start dawarich\` to start the pod."
