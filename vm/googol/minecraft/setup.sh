2ac8246f28d37acdbe25aae03711e440dfcffb4f
Arístides Pérez <aristp.2005@gmail.com>
2026-07-18 04:43:28 -0400

    chore: configure minecraft packets routing from local and googol to allow public access

--- vm/googol/minecraft/setup.sh
+++ vm/googol/minecraft/setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo "Setting up NAT..."
-iptables -t nat -A POSTROUTING -s 10.0.0.0/16 -o eth0 -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 10.0.0.0/16 -o tailscale0 -j MASQUERADE
-
 echo "Setting up Minecraft NAT..."
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 25565 -j DNAT --to-destination 10.0.4.4:25565
+iptables -t nat -A PREROUTING -i ens4 -p tcp --dport 25565 -j DNAT --to-destination 10.0.4.4:25565
+iptables -t nat -A POSTROUTING -o tailscale0 -p tcp -d 10.0.4.4 --dport 25565 -j MASQUERADE
+iptables -A FORWARD -p tcp -d 10.0.4.4 --dport 25565 -j ACCEPT
 
 echo "Saving iptables rules..."
 apt update && apt upgrade -y && apt install -y iptables-persistent && netfilter-persistent save
@@ -14,4 +12,4 @@ echo "Configuring IP forwarding..."
 printf "net.ipv4.ip_forward = 1\nnet.ipv6.conf.all.forwarding = 1\n" | tee /etc/sysctl.d/99-ip-forwarding.conf
 sysctl -p /etc/sysctl.d/99-ip-forwarding.conf
 
-echo "Gateway setup complete."
+echo "Minecraft TCP 25565 forwarding setup complete."
